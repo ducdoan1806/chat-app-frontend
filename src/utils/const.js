0b5c0ab8d14106ef39641df1afff59f619ab03d4
ducdoan1806 <ducdoan1806@gmail.com>
2024-06-26 13:40:19 +0700

    update

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,4 +1,4 @@
-export const CLIENT_ID = "74TSugHJDzUbdUSthGotqZY4WcyrdDLsq0GwumHe";
+export const CLIENT_ID = "jpW9lupnaDlmy6HZxIR6wxA6CjlTX3T7pKrxkDed";
 export const CLIENT_SECRET =
-  "VQnu25o3gmiRKh0iesi65sSkdwWW6c4tAX1eAwXb6Y4rFpIaibA69Inz56bn26uRrTw8Ftc2wbhV9pCQMTIArX9MFIOp0k7Fl5TZGNJjwCjsbTYS9sUKMsH1LRpPkQcL";
+  "lPkAPCX8x8UzeLDPy8M0X0a2mRxKKv5nrvFuKcS3ENVeBwDTAIKpw7NxAmgKIYR65Fmh80cedjRWEUsSZSkQ3nxK8MBC9xxoLoBNfouEMuI5evF9Povigyhxgm9qslsY";
 export const GRANT_TYPE = "password";
